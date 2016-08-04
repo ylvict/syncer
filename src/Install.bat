fda338e4dde7f8876c10cf23ee52f137a1e73333
Hover Zuo <ylvict@gmail.com>
2016-08-04 15:23:14 +0800

    Add installer;

--- src/Install.bat
+++ src/Install.bat
@@ -1,3 +1,3 @@
-﻿"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe" Syncer.exe
+"%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe" Syncer.exe
 net start Syncer
 sc config Syncer start= auto
